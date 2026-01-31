013dc79e603751cd7727f91c562275e7a881b6d7
RruizVM <rz.remy@outlook.fr>
2026-01-31 11:46:29 +0100

    hunt and kill algo creation

---
+++ src/maze/algorithms/hunt_and_kill.py
@@ -0,0 +1,20 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  hunt_and_kill.py                                  :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: roandrie, rruiz                           +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/01/31 10:55:56 by rruiz           #+#    #+#               #
+#  Updated: 2026/01/31 11:06:16 by rruiz           ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+import random
+
+from typing import Any
+
+from src.maze.maze_customization import MAZE
+
+def hunt_and_kill():
+    
