32e14c0cda466b8a3699527979cfedab3944734a
RruizVM <rz.remy@outlook.fr>
2026-01-31 16:28:53 +0100

    hunt and kill algo creation

--- src/maze/algorithms/hunt_and_kill.py
+++ src/maze/algorithms/hunt_and_kill.py
@@ -6,15 +6,18 @@
 #  By: roandrie, rruiz                           +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/01/31 10:55:56 by rruiz           #+#    #+#               #
-#  Updated: 2026/01/31 11:06:16 by rruiz           ###   ########.fr        #
+#  Updated: 2026/01/31 15:15:24 by rruiz           ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
-import random
+from random import randint
 
 from typing import Any
 
 from src.maze.maze_customization import MAZE
 
-def hunt_and_kill():
-    
+def hunt_and_kill(generator: Any, rendering: bool) -> None:
+    visited_cases = []
+    current_x = randint(0, generator.width + 1)
+    current_y = randint(0, generator.height + 1)
+    current_coord = (current_x, current_y)
